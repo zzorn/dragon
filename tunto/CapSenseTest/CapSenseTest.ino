ae43fcb94a541f13af752d026ab44aeee786ecee
shiera@moonshadow <shiera>
2013-06-22 21:08:32 +0300

    tested sensor

--- tunto/CapSenseTest/CapSenseTest.ino
+++ tunto/CapSenseTest/CapSenseTest.ino
@@ -15,9 +15,11 @@ CapacitiveSensor   cs_4_5 = CapacitiveSensor(4,5);        // 10 megohm resistor 
 CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10 megohm resistor between pins 4 & 8, pin 8 is sensor pin, add wire, foil
 
 int led = 13;
+int ledG = 12;
 void setup()                    
 {
    pinMode(led, OUTPUT);
+   pinMode(ledG, OUTPUT);
    //cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
 
@@ -28,23 +30,30 @@ void loop()
     long start = millis();
     long total1 =  cs_4_2.capacitiveSensor(30);
     //long total2 =  cs_4_5.capacitiveSensor(30);
-    //long total3 =  cs_4_8.capacitiveSensor(30);
+     long total2 =  cs_4_8.capacitiveSensor(30);
 
     Serial.print(millis() - start);        // check on performance in milliseconds
     Serial.print("\t");                    // tab character for debug windown spacing
 
-    Serial.println(total1);                  // print sensor output 1
-    /*Serial.print("\t");
-    Serial.println(total2);                  // print sensor output 2
+    Serial.print(total1);                  // print sensor output 1
     Serial.print("\t");
+    Serial.println(total2);                  // print sensor output 2
+    /*Serial.print("\t");
     Serial.println(total3);       // print sensor output 3
     */
-    if (total1 >= 2) {
+    if (total1 >= 70) {
       digitalWrite(led, LOW);
       
     }  
     else{
       digitalWrite(led, HIGH); 
+    }  
+    if (total2 >= 70) {
+      digitalWrite(ledG, LOW);
+      
+    }  
+    else{
+      digitalWrite(ledG, HIGH); 
     }  
     delay(100);                             // arbitrary delay to limit data to serial port 
 }
